721ec1b2400bcb5d251d346983ff31861201b497
Gautam Bansal <gautam.bansal@toddleapp.com>
2025-08-04 14:27:44 +0530

    chore: add react compiler support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 import execute from "./plugins/rollup-execute.js";
+import { babel } from "@rollup/plugin-babel";
+import ts from "typescript";
 
 // Determine if we are in watch mode
 const isDevelopment = process.env.ROLLUP_WATCH === "true";
@@ -28,7 +30,16 @@ export default {
       extensions: [".js", ".jsx", ".ts", ".tsx"],
     }),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    babel({
+      babelHelpers: "bundled",
+      include: ["src/**/*"],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      exclude: ["node_modules/**"],
+    }),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      typescript: ts,
+    }),
     postcss({
       extract: "styles.css",
       modules: true,
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Button.scss";
 
 const Button = () => {
-  return <div className={classes.container}>Button2</div>;
+  return <div className={classes.button}>Button</div>;
 };
 
 export default Button;
